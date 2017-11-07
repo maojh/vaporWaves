607a35fde728302bd898fe8126d93b25df91ea54
maojh <mjh.ilbasco@gmail.com>
2017-11-08 00:46:13 +0100

    update sketch

--- sketch.js
+++ sketch.js
@@ -4,19 +4,31 @@
 
 var song, amplitude;
 var moveY = 0;
-var picco = -50;
-var passo = 40;
+var picco = -100;
+var passo;
 var s = 0.4; //scala
 
+var trasback = 2;
+var light = true;
+
+var lines = new Array();
+var pictures = new Array();
+var smalls = new Array();
+
 function preload() {
-
   song = loadSound('assets/U.mp3');
-
-  //nuvole
-  // nuvola1 = loadImage("immagini/nuvola1.jpg");
-  // nuvola2 = loadImage("immagini/nuvola2.jpg");
-  // nuvola3 = loadImage("immagini/nuvola3.jpg");
-
+  //lines
+  for (var i = 1; i <= 4; i++) {
+    lines.push(loadImage("assets/lines/"+ 1 + ".png"));
+  }
+  //pictures
+  for (var i = 1; i <= 12; i++) {
+    pictures.push(loadImage("assets/pictures/"+ i + ".png"));
+  }
+  for (var i = 1; i <= 11; i++) {
+    smalls.push(loadImage("assets/smalls/"+ i + ".png"));
+  }
+  smalls.push(loadImage("assets/smalls/"+ 12 + ".gif"));
 }
 
 
@@ -32,42 +44,47 @@ function setup() {
 
   // responsive unit
   u = (windowWidth/windowHeight);
-
+  passo = int(35*u);
+  console.log(passo);
   noStroke();
   textSize(24);
   scale(0.1);
+
+  frameRate(15);
 }
 
 function draw() {
-  fill(2, 1);
   noStroke();
-  rect(0, 0, width, height);
+  push();
+    fill(2,2);
+    rect(0,0, width, height);
+  pop();
+
+  if(millis()/1000>60) light = false;
+
+  sfondo(light);
 
   //display test
-    fill(20, 255);
-    noStroke();
-    rect(85, 10, 100, 40);
-    fill(0,255,0,255);
-    text(int(millis()/1000), 100, 40);
+  fill(20, 255);
+  noStroke();
+  rect(85, 10, 100, 40);
+  fill(0,255,0,255);
+  text(int(millis()/1000), 100, 40);
 
 
   // analysis
   //aplitude
   var level = amplitude.getLevel();
-  //display amplitude, sole?
-  var size = map(level, 0, 1, 50, 500);
-  var levelcolor = map(level, 0, 1, 220, 255);
-  fill(levelcolor, levelcolor, 0, 255);
-  noStroke();
-  ellipse(width/4, height/4, size, size);
 
-  translate(0, height/2.7); // /2.5 horizon
+  sole(level);
+
+  translate(0, height/3.3); // /2.5 horizon
   moveY += map(level, 0, 1, 0, 50);
   if (moveY >= height) {
     moveY = 0; //partenza
     s = 0;
-
   }
+
   translate(width/2, moveY);
   scale(s+=0.007);
 
@@ -81,100 +98,119 @@ function draw() {
   //     rect(x, height, width / spectrum.length, h )
   //   }
 
-  fill(255,0,255, 50);
-  stroke(255, 240, 255);
-//   stroke(levelcolor, random(levelcolor), random(levelcolor)); // waveform is red
-   strokeWeight(0.5);
-   //sinistra
-   beginShape();
-   for (var i = 0; i< spectrum.length; i+=passo){
-     var x1 = map(i, 0, spectrum.length, -width/2, passo);
-     var y1 = map( spectrum[i], 0, 255, -picco, picco);
-     vertex(x1,y1);
-   }
-   endShape();
-   beginShape();
-   for (var i = 0; i< spectrum.length; i+=passo){
-     var x2 = map(i, 0, spectrum.length, -width/2, -width);
-     var y2 = map( spectrum[i], 0, 255, -picco, picco);
-     vertex(x2,y2);
-   }
-   vertex(x2-1000, y2);
-   endShape();
-    //destra
-    push();
-      translate(width-passo/2, 0);
-      beginShape();
-      for (var i = 0; i< spectrum.length; i+=passo){
-        var x2 = map(i, 0, spectrum.length, -width/2, -width);
-        var y2 = map( spectrum[i], 0, 255, -picco, picco);
-        vertex(x2,y2);
-      }
-      endShape();
-      beginShape();
-      for (var i = 0; i< spectrum.length; i+=passo){
-        var x1 = map(i, 0, spectrum.length, -width/2, -passo);
-        var y1 = map( spectrum[i], 0, 255, -picco, picco);
-        vertex(x1,y1);
-      }
-      vertex(x1+1000, y1);
-      endShape();
-    pop();
-
-
-
-
-  //  beginShape();
-  //  for (var i = spectrum.length; i > 0; i-=passo){
-  //    var x = map(i, spectrum.length, 0+passo, passo/2.6, width/2);
-  //    var y = map( spectrum[i], 0, 255, -picco, picco);
-  //    vertex(x,y);
-  //  }
-  // endShape();
-  // beginShape();
-  //   for(var i = 0; i< spectrum.length; i+=passo){
-  //      var x = map(i, 0, spectrum.length, width/2+passo/4, width);
-  //      var y = map( spectrum[i], 0, 255, -picco, picco);
-  //     vertex(x,y);
-  //    }
-  //  pop();
-  // endShape();
-
-
-
-  // var waveform = fft.waveform();
-  // noFill();
-  // beginShape();
-  //  stroke(255,0,0); // waveform is red
-  //  strokeWeight(1);
-  //  for (var i = 0; i< waveform.length; i++){
-  //    var x = map(i, 0, waveform.length, 0, width);
-  //    var y = map( waveform[i], -1, 1, 0, height);
-  //    vertex(x,y);
-  //  }
-  // endShape();
+  if (light) {
+    fill(0,0,55, 50);
+    stroke(255, 255);
+  } else {
+    fill(0,0,55, 50);
+    stroke(55, 255);
+  }
+  //   stroke(levelcolor, random(levelcolor), random(levelcolor)); // waveform is red
+  strokeWeight(2);
+  //sinistra
+  beginShape();
+  for (var i = 0; i< spectrum.length; i+=passo){
+    var x1 = map(i, 0, spectrum.length, -width/2, passo);
+    var y1 = map( spectrum[i], 0, 255, -picco, picco);
+    vertex(x1,y1);
+  }
+  endShape();
+  beginShape();
+  for (var i = 0; i< spectrum.length; i+=passo){
+    var x2 = map(i, 0, spectrum.length, -width/2, -width);
+    var y2 = map( spectrum[i], 0, 255, -picco, picco);
+    vertex(x2,y2);
+  }
+  vertex(x2-1000, y2);
+  endShape();
+  //destra
+  push();
+  translate(width-passo/2, 0);
+  beginShape();
+  for (var i = 0; i< spectrum.length; i+=passo){
+    var x2 = map(i, 0, spectrum.length, -width/2, -width);
+    var y2 = map( spectrum[i], 0, 255, -picco, picco);
+    vertex(x2,y2);
+  }
+  endShape();
+  beginShape();
+  for (var i = 0; i< spectrum.length; i+=passo){
+    var x1 = map(i, 0, spectrum.length, -width/2, -passo);
+    var y1 = map( spectrum[i], 0, 255, -picco, picco);
+    vertex(x1,y1);
+  }
+  vertex(x1+1000, y1);
+  endShape();
+  pop();
 
 }
 
+function sole(level) {
+  var size = map(level, 0, 1, 50*u, 500*u);
+  var levelcolor = map(level, 0, 1, 220, 255);
+  fill(levelcolor, levelcolor, 0, 255);
+  noStroke();
+  ellipse(width/2, height/4, size, size);
 
+}
 
-
+function sfondo(light) {
+  var val;
+  var dens = 10*u;
+  var seconds = millis()/1000;
+  // if(seconds < 4) {
+  //   if(trasback>1) {
+  //     trasback-=5;
+  //   }
+  //   // ramp down - intro
+  // }
+  // if( seconds > 110) {
+  //   if(trasback>width/1500) {
+  //     trasback+=5;
+  //   }
+  //   //ramp up - conclusione
+  // }
+push();
+  if(light) {
+  for (var i = 0; i < height; i+=dens) {
+    if(i<height/3) {
+      val = int(map(i, 0, height/6, 245, 180));
+      fill(val, 0, val/2, trasback*10);
+    } else {
+      val = int(map(i, height/3, height, 50, 205));
+      fill(0, val/4, val, trasback);
+    }
+    rect(0, i, width, dens);
+  }
+} else {
+  for (var i = 0; i < height; i+=dens) {
+    if(i<height/3) {
+      val = map(i, 0, height/4, 180, 245)/2;
+      fill(val, 0, val/2, trasback*10);
+    } else {
+      val = map(i, height/4, hleight, 180, 245)/2;
+      fill(0, val/4, val, trasback);
+    }
+    rect(0, i, width, dens);
+  }
+}
+pop();
+}
 //Utilità
 //
 //Screenshot function
 function mousePressed() {
-    // save();
-
+  save("cover.png");
   //play/pause song loop
   push();
   fill(0,255,0);
   noStroke();
   if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-      song.pause(); // .play() will resume from .pause() position
-        text("Pause", 100, 30);
+    song.pause(); // .play() will resume from .pause() position
+    text("Pause", 100, 30);
   } else {
     song.play();
-      text("Play", 100, 30);
+    text("Play", 100, 30);
   }
   pop();
 }
